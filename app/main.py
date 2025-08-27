0cf4393b49f3e3eff8027cbe8d135361d689bcc8
Ana Karolina <anakarolina538@gmail.com>
2025-08-27 16:43:14 -0300

    solving task

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
-from typing import Callable
+from typing import Callable, Any, Tuple
 
 
-def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+def cache(func: Callable[..., Any]) -> Callable[..., Any]:
+    _cache: dict[Tuple[Any, ...], Any] = {}
+
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        key: Tuple[Any, ...] = args + tuple(
+            sorted(kwargs.items())
+        )
+        if key in _cache:
+            print("Getting from cache")
+            return _cache[key]
+
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        _cache[key] = result
+        return result
+
+    return wrapper
