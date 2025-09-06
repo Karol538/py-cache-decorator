ebfdffca4f655bbdf2061475d8222d42962e4b20
Ana Karolina <anakarolina538@gmail.com>
2025-09-06 12:03:58 -0300

    solving task

--- app/main.py
+++ app/main.py
@@ -12,8 +12,9 @@ def is_hashable(obj: Any) -> bool:
 def cache(func: Callable[..., Any]) -> Callable[..., Any]:
     _global_cache: Dict[Callable[..., Any], Dict[Tuple[Any, ...], Any]] = {}
 
-    def wrapper(*args: Any) -> Any:
-        for arg in args:
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        # Verifica se todos os argumentos são hashable
+        for arg in list(args) + list(kwargs.values()):
             if not is_hashable(arg):
                 raise TypeError(
                     f"O argumento {arg} não é imutável ou hashable. "
@@ -21,7 +22,10 @@ def cache(func: Callable[..., Any]) -> Callable[..., Any]:
                     "strings ou números."
                 )
 
-        key: Tuple[Any, ...] = args
+        # Cria chave única e ordenada para kwargs
+        key: Tuple[Any, ...] = args + tuple(sorted(kwargs.items()))
+
+        # Cache isolado por função decorada
         func_cache = _global_cache.setdefault(func, {})
 
         if key in func_cache:
@@ -29,8 +33,7 @@ def cache(func: Callable[..., Any]) -> Callable[..., Any]:
             return func_cache[key]
 
         print("Calculating new result")
-        result = func(*args)
+        result = func(*args, **kwargs)
         func_cache[key] = result
         return result
-
     return wrapper
